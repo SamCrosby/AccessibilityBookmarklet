127e224259f15081e9e3e7992f61e5db0365c35f
SamCrosby <SamanthaCrosby97@Outlook.com>
2018-03-28 13:24:30 +0100

    Add files via upload
    All but one error alert (duplicate images) have been replaced with boxes that display the error.

--- AccessibilityFramework2/AccessibilityFramework2/AccessibilityBookmarklet.js
+++ AccessibilityFramework2/AccessibilityFramework2/AccessibilityBookmarklet.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
+
   $("img").attr("alt", function() {
       var alt = $(this).attr("alt");
       if(alt == "") {
-	      alert("Image missing alternative text");
+	      // alert("Image missing alternative text");
+        $("<div>Image missing alternative text.</div>").insertAfter(this).addClass("error");
       }
 
       var alternativeList = [];
@@ -19,13 +21,15 @@ $(document).ready(function() {
         var alter = $(this).attr("alt");
         var tally = 0;
 
+        // alert(alternativeList.length);
+
         for (i = 0; i < alternativeList.length; i++ ) {
             if (alternativeList[i] === alter){
-
             tally = tally + 1;
               // alert(tally);
                 if (tally >= 2) {
                   alert("Image has alternative text duplicate");
+                  // $("<div>Image has alternative text duplicate.</div>").insertAfter("img").addClass("error");
                   break
                 }
           } else {
@@ -60,13 +64,15 @@ $(document).ready(function() {
               for (i = 0; i < idList2.length; i++) {
                   if (idList2[i] === id){
                   tally = tally + 1;
-                  alert(tally);
+                  // alert(tally);
                       if (tally >= 2) {
-                        alert("There are duplicate id's.");
+                        // alert("There are duplicate id's.");
+                        $("<div>There are duplicate id's.</div>").insertAfter(this).addClass("error");
+
                         break
                       }
                 } else {
-                  alert(idList2);
+                  // alert(idList2);
                 }
               }
             }
@@ -79,45 +85,55 @@ $(document).ready(function() {
     $("a").attr("href", function() {
         var link = $(this).attr("href");
         if(link == "") {
-          alert("Link Error");
+          // alert("Link is empty.");
+          $("<div>Link is empty.</div>").insertAfter(this).addClass("error");
         }
     });
+
+
   $("html").attr("lang", function() {
       var lang = $(this).attr("lang");
       if(lang == "") {
-        alert("Missing language");
+        // alert("Missing language");
+        $("<div>Missing document language.</div>").appendTo("body").addClass("error");
       }
   });
+
 
   $("button").html(function() {
       var button_text = $(this).html();
       if(button_text == "") {
-        alert("Missing button text");
-      }
-  });
-  $(":header").html(function() {
-      var header = $(this).html();
-      if(header == "") {
-        alert("Missing heading text");
+        // alert("Missing button text");
+        $("<div>Missing button text.</div>").insertAfter(this).addClass("error");
       }
   });
 
+
+  $(":header").html(function() {
+      var header = $(this).html();
+      if(header == "") {
+        // alert("Missing heading text");
+        $("<div>Missing heading text.</div>").insertAfter(this).addClass("error");
+      }
+  });
+
+
    $( "li", document.body ).each(function() {
      var parentTag = $( this ).parent().get( 0 ).tagName;
-     if(parentTag == "UL", parentTag == "OL") {
-       alert("okay");
-     } else {
-       alert("List is not contained within <ul> or <ol> parent element.")
-     }
+     if(parentTag !== "UL", parentTag !== "OL") {
+       // alert("List is not contained within <ul> or <ol> parent element.")
+       $("<div>List is not contained within ul or ol parent elements.</div>").insertAfter(this).addClass("error");
+    }
    });
+
 
    $( "input", document.body ).each(function() {
      var siblingTag = $( this ).siblings().get( 0 ).tagName;
      // alert(siblingTag);
-     if(siblingTag == "LABEL") {
-       alert("okay");
-     } else {
-       alert("Form is missing a label")
+     if(siblingTag !== "LABEL") {
+       // alert("Form is missing a label")
+       $("<div>Form is missing a label.</div>").insertAfter(this).addClass("error");
+
      }
    });
 });
