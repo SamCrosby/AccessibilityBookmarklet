94b47df9b861739d72e4fa67bf228f18b30b7c8a
SamCrosby <SamanthaCrosby97@Outlook.com>
2018-03-29 12:34:44 +0100

    Add files via upload
    Error boxes now work properly, with styling included.

--- AccessibilityFramework2/AccessibilityBookmarklet.js
+++ AccessibilityFramework2/AccessibilityBookmarklet.js
@@ -1,45 +1,71 @@
 $(document).ready(function() {
 
+
+//This is the function
   $("img").attr("alt", function() {
       var alt = $(this).attr("alt");
+
+      //This is the if statement that checks if the alt tags are empty. If they are,
+      //then an error box appears with the message "Image missing alternative text."
       if(alt == "") {
 	      // alert("Image missing alternative text");
         $("<div>Image missing alternative text.</div>").insertAfter(this).addClass("error");
+        $(".error").css({
+          'background-color': 'red',
+          'color': 'white',
+          'width': '100px',
+          'padding': '5px',
+          'border': 'none',
+          'margin': '0',
+          'font-family':'sans-serif',
+        });
+
       }
 
+      //This is the array that contains the list of all the alternative text.
       var alternativeList = [];
 
-
-      $("img").each(function(){
-        var alts = $(this).attr("alt");
-        alternativeList.push(alts);
-        // alert(alternativeList);
-    });
-
-
-    $("img").attr("alt", function() {
-        var alter = $(this).attr("alt");
-        var tally = 0;
-
-        // alert(alternativeList.length);
-
-        for (i = 0; i < alternativeList.length; i++) {
-            if (alternativeList[i] === alter){
-              // alert(alternativeList[i]);
-              tally = tally + 1;
-              // alert(tally);
-                if (tally >= 2) {
-                  // alert("Image has alternative text duplicate");
-                  $("<div>Image has alternative text duplicate.</div>").insertAfter("img").addClass("error");
-                  break;
-                }
-              }
-           else {
-             // alert(alternativeList);
-          }
-
-        }
-      });
+      //This function goes through each image, retrieves the alternative Text
+      // and then pushes it to the alternativeList array.
+    //   $("img").each(function(){
+    //     var alts = $(this).attr("alt");
+    //     alternativeList.push(alts);
+    //     // alert(alternativeList);
+    // });
+    //
+    //
+    // $("img").attr("alt", function() {
+    //     var alter = $(this).attr("alt");
+    //     var tally = 0;
+    //
+    //     // alert(alternativeList.length);
+    //
+    //     for (i = 0; i < alternativeList.length; i++) {
+    //         if (alternativeList[i] === alter){
+    //           // alert(alternativeList[i]);
+    //           tally = tally + 1;
+    //           // alert(tally);
+    //             if (tally >= 2) {
+    //               // alert("Image has alternative text duplicate");
+    //               $("<div>Image has alternative text duplicate.</div>").insertAfter("img").addClass("error");
+                    // $(".error").css({
+                    //   'background-color': 'red',
+                    //   'color': 'white',
+                    //   'width': '100px',
+                    //   'padding': '5px',
+                    //   'border': 'none',
+                    //   'margin': '0',
+                    //   'font-family':'sans-serif',
+                    // });
+                    //  break;
+    //             }
+    //           }
+    //        else {
+    //          // alert(alternativeList);
+    //       }
+    //
+    //     }
+    //   });
       });
 
 
@@ -71,7 +97,15 @@ $(document).ready(function() {
                       if (tally >= 2) {
                         // alert("There are duplicate id's.");
                         $("<div>There are duplicate id's.</div>").insertAfter(this).addClass("error");
-
+                        $(".error").css({
+                          'background-color': 'red',
+                          'color': 'white',
+                          'width': '100px',
+                          'padding': '5px',
+                          'border': 'none',
+                          'margin': '0',
+                          'font-family':'sans-serif',
+                        });
                         break
                       }
                 } else {
@@ -89,7 +123,16 @@ $(document).ready(function() {
         var link = $(this).attr("href");
         if(link == "") {
           // alert("Link is empty.");
-          $("<div>Link is empty.</div>").insertAfter(this).addClass("error");
+          $("<div>Link is empty.</div>").prependTo(this).addClass("error");
+          $(".error").css({
+            'background-color': 'red',
+            'color': 'white',
+            'width': '100px',
+            'padding': '5px',
+            'border': 'none',
+            'margin': '0',
+            'font-family':'sans-serif',
+          });
         }
     });
 
@@ -99,6 +142,15 @@ $(document).ready(function() {
       if(lang == "") {
         // alert("Missing language");
         $("<div>Missing document language.</div>").appendTo("body").addClass("error");
+        $(".error").css({
+          'background-color': 'red',
+          'color': 'white',
+          'width': '100px',
+          'padding': '5px',
+          'border': 'none',
+          'margin': '0',
+          'font-family':'sans-serif',
+        });
       }
   });
 
@@ -108,15 +160,35 @@ $(document).ready(function() {
       if(button_text == "") {
         // alert("Missing button text");
         $("<div>Missing button text.</div>").insertAfter(this).addClass("error");
+        $(".error").css({
+          'background-color': 'red',
+          'color': 'white',
+          'width': '100px',
+          'padding': '5px',
+          'border': 'none',
+          'margin': '0',
+          'font-family':'sans-serif',
+        });
       }
   });
 
-
+  //This function retrives all the text from the headers.
+  //If the header text is empty, an error box appears after the headers
+  //with the message "Missing heading text.".
   $(":header").html(function() {
       var header = $(this).html();
       if(header == "") {
         // alert("Missing heading text");
         $("<div>Missing heading text.</div>").insertAfter(this).addClass("error");
+        $(".error").css({
+          'background-color': 'red',
+          'color': 'white',
+          'width': '100px',
+          'padding': '5px',
+          'border': 'none',
+          'margin': '0',
+          'font-family':'sans-serif',
+        });
       }
   });
 
@@ -125,7 +197,16 @@ $(document).ready(function() {
      var parentTag = $( this ).parent().get( 0 ).tagName;
      if(parentTag !== "UL", parentTag !== "OL") {
        // alert("List is not contained within <ul> or <ol> parent element.")
-       $("<div>List is not contained within ul or ol parent elements.</div>").insertAfter(this).addClass("error");
+       $("<div>List is not contained within ul or ol parent elements.</div>").prependTo(this).addClass("error");
+       $(".error").css({
+         'background-color': 'red',
+         'color': 'white',
+         'width': '100px',
+         'padding': '5px',
+         'border': 'none',
+         'margin': '0',
+         'font-family':'sans-serif',
+       });
     }
    });
 
@@ -136,24 +217,57 @@ $(document).ready(function() {
      if(siblingTag !== "LABEL") {
        // alert("Form is missing a label")
        $("<div>Form is missing a label.</div>").insertAfter(this).addClass("error");
-     }
+       $(".error").css({
+         'background-color': 'red',
+         'color': 'white',
+         'width': '100px',
+         'padding': '5px',
+         'border': 'none',
+         'margin': '0',
+         'font-family':'sans-serif',
+       });
+   }
 
    });
 
    $("form", document.body).each(function() {
-
      var input = $(this).children("input");
      var labels = $(this).children("label");
 
      if(labels.length > input.length) {
        $("<div>Multiple form labels.</div>").insertAfter(this).addClass("error");
+       $(".error").css({
+         'background-color': 'red',
+         'color': 'white',
+         'width': '100px',
+         'padding': '5px',
+         'border': 'none',
+         'margin': '0',
+         'font-family':'sans-serif',
+       });
      }
 
    });
 
 
+   $("html, body, p, h1, h2, h3, h4, h5, h6, a").each( function() {
+     var size = $(this).css('font-size')
+     var remove = size.replace(/px/gi, "");
 
-
+     if(remove < 10) {
+       $("<div>Text too small.</div>").insertAfter(this).addClass("error");
+       $(".error").css({
+         'background-color': 'red',
+         'color': 'white',
+         'width': '100px',
+         'padding': '5px',
+         'border': 'none',
+         'margin': '0',
+         'font-family':'sans-serif',
+       });
+     }
+     // alert(fontSizes);
+   });
 
 
 });
